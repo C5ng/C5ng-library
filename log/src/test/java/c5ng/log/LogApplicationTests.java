3db096f0bbe61c4583eb2fb5a93f205bd7ce01a1
공대표 <c5ngsdp@gmail.com>
2024-04-17 18:19:35 +0900

    Project "log" Initial Commit

---
+++ log/src/test/java/c5ng/log/LogApplicationTests.java
@@ -0,0 +1,13 @@
+package c5ng.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
